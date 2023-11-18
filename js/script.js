19024a12d79a35f3432e05a8fbf4a34eea726fc4
Pradeep Kumar Gupta <100694988+Pradeepgupta77@users.noreply.github.com>
2023-11-18 14:56:17 +0530

    navbar complite

--- js/script.js
+++ js/script.js
@@ -12,3 +12,111 @@ window.addEventListener("load", () => {
         dayNight.querySelector("i").classList.add("fa-moon")
     }
 })
+
+// ****
+// document.addEventListener("DOMContentLoaded", function () {
+//     var navToggle = document.querySelector(".nav-toggle");
+//     var navOpen = document.querySelector(".navopen");
+//     var navClose = document.querySelector(".naclose");
+//     var aside = document.querySelector(".aside");
+
+//     function handleToggle() {
+//         var windowWidth = window.innerWidth;
+//         if (windowWidth > 1199) {
+//             navToggle.classList.add("hidden");
+//         } else {
+//             navToggle.classList.remove("hidden");
+//         }
+//     }
+
+//     function handleClick() {
+//         navOpen.addEventListener("click", function () {
+//             navOpen.classList.add("hidden");
+//             navClose.classList.remove("hidden");
+//             aside.classList.add("mobo-nav-open");
+//         });
+
+//         navClose.addEventListener("click", function () {
+//             navClose.classList.add("hidden");
+//             navOpen.classList.remove("hidden");
+//             aside.classList.remove("mobo-nav-open");
+//         });
+//     }
+
+//     // Initial setup
+//     handleToggle();
+//     handleClick();
+
+//     // Handle window resize
+//     window.addEventListener("resize", function () {
+//         handleToggle();
+//     });
+// });
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle window resize
+    function handleResize() {
+        var windowWidth = window.innerWidth;
+
+        // Check if window width is above 1199px
+        if (windowWidth > 1199) {
+            // Add 'hidden' class to nav-toggle
+            document.querySelector('.nav-toggle').classList.add('hidden');
+        } else {
+            // Remove 'hidden' class from nav-toggle
+            document.querySelector('.nav-toggle').classList.remove('hidden');
+        }
+    }
+
+    // Function to handle click on navopen
+    function handleNavOpenClick() {
+        // Add 'hidden' class to navopen
+        document.querySelector('.navopen').classList.add('hidden');
+
+        // Remove 'hidden' class from naclose
+        document.querySelector('.naclose').classList.remove('hidden');
+
+        // Add 'mobo-nav-open' class to aside
+        document.querySelector('.aside').classList.add('mobo-nav-open');
+    }
+
+    // Function to handle click on naclose
+    function handleNacloseClick() {
+        // Add 'hidden' class to naclose
+        document.querySelector('.naclose').classList.add('hidden');
+
+        // Remove 'hidden' class from navopen
+        document.querySelector('.navopen').classList.remove('hidden');
+
+        // Remove 'mobo-nav-open' class from aside
+        document.querySelector('.aside').classList.remove('mobo-nav-open');
+    }
+
+    // Function to handle click on li in the nav
+    function handleNavLiClick() {
+        // Remove 'mobo-nav-open' class from aside
+        document.querySelector('.aside').classList.remove('mobo-nav-open');
+
+        // Remove 'hidden' class from navopen
+        document.querySelector('.navopen').classList.remove('hidden');
+
+        // Add 'hidden' class to naclose
+        document.querySelector('.naclose').classList.add('hidden');
+    }
+
+    // Event listeners
+    window.addEventListener('resize', handleResize);
+    document.querySelector('.navopen').addEventListener('click', handleNavOpenClick);
+    document.querySelector('.naclose').addEventListener('click', handleNacloseClick);
+
+    // Get all li elements in the nav and add click event listener
+    var navLiElements = document.querySelectorAll('.nav li');
+    navLiElements.forEach(function (li) {
+        li.addEventListener('click', handleNavLiClick);
+    });
+
+    // Initial resize handling
+    handleResize();
+});
